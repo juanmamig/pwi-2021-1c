b68538341b4a49c6e136ae56562319dcc1b83d36
Juan Ma <juanmariamigliore@gmail.com>
2021-06-07 16:40:02 -0300

    Clase 10

---
+++ Clase 10/condicionales/main.js
@@ -0,0 +1,7 @@
+let edad = prompt("Ingrese su edad");
+console.log(typeof edad); //string
+// edad = parseInt(edad, 10);
+edad = Number(edad);
+console.log(typeof edad); //number
+console.log(edad + 10); // la suma de edad + 10
+
