487d7adb8948c966e422f36a1c87b84b34748c06
Juan Ma <juanmariamigliore@gmail.com>
2021-06-09 17:16:35 -0300

    Arrays

---
+++ Clase 11/arrays/main.js
@@ -0,0 +1,12 @@
+const miArray = ["Juan", 29, true, undefined];
+console.log(miArray[2]);
+
+const matrix = ["Juan", [true, "Pedro"], 29];
+console.log(matrix[1][1]);
+
+console.log(matrix[10]);
+
+console.log(matrix.length); //3
+
+let nombres = ["Juan", "Mercedes", "Sofía", "Lucas", "Luca"];
+console.log(nombres.length); //
