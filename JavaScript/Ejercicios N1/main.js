7428997f8d81f004e494ec896f43aa9a5249c624
Juan Ma <juanmariamigliore@gmail.com>
2021-06-09 14:59:33 -0300

    Ejercicios N1

---
+++ JavaScript/Ejercicios N1/main.js
@@ -0,0 +1,93 @@
+/* Pedirle al usuario que ingrese un número y diga si el número es par o impar. */
+
+// let edad = prompt("Ingrese su edad");
+// edad = Number(edad);
+// if (edad % 2 === 0) {
+//   console.log("Es Par");
+// } else {
+//   console.log("Es impar");
+// }
+
+/* Pedirle al usuario que ingrese dos números y diga si el primer número es divisible
+por el segundo. */
+
+// let num1 = prompt("Ingrese un número");
+// num1 = Number(num1);
+// let num2 = prompt("Ingrese otro número");
+// if (num1 % num2 === 0) {
+//   console.log("El primer número es divisible por el segundo");
+// } else {
+//   console.log("El primer número no es divisible por el segundo");
+// }
+
+/*
+Armar un programa que pida un usuario y una contraseña. Si la contraseña es
+correcta, dejarlo pasar, sino decirle que no puede entrar.
+*/
+
+// const pass = "1234";
+// prompt("Ingrese su usuario");
+// const userPass = prompt("Ingrese la contraseña");
+// num1 = Number(num1);
+// if (userPass === pass) {
+//   console.log("Puede pasar");
+// } else {
+//   console.log("No puede pasar")
+// }
+
+/**
+ *
+Entrada a un bar - sector VIP:
+a. Pedirle la edad al usuario
+b. Si tiene más de 18 años, pedirle la clave
+c. Si la clave es correcta, dejarlo pasar al sector vip
+d. Si la clave es incorrecta no puede pasar al sector vip
+e. Si tiene menos de 18 años no puede pasar.
+ */
+
+
+// Opción 1
+// const clave = "1234";
+// let edad = prompt("Ingrese su edad");
+// edad = Number(edad);
+
+// if (edad > 18) {
+//   const userClave = prompt("Ingrese la clave");
+//   if (userClave === clave) {
+//     console.log("Puede pasar al sector VIP");
+//   } else {
+//     console.log("No puede pasar al sector VIP");
+//   }
+// } else {
+//   console.log("No puede pasar, su edad es muy chica");
+// }
+
+// Opción 2
+
+// const clave = "1234";
+// let edad = prompt("Ingrese su edad");
+// edad = Number(edad);
+// const userClave = prompt("Ingrese la clave");
+
+// if (edad <= 18) {
+//   console.log("No puede pasar, su edad es muy chico");
+// } else if (userClave === clave) {
+//   console.log("Puede pasar al sector VIP");
+// } else {
+//   console.log("No puede pasar al sector VIP");
+// }
+
+/* Refactorizar el punto 3, usando el operador lógico && */
+
+// const clave = "1234";
+// let edad = prompt("Ingrese su edad");
+// edad = Number(edad);
+// const userClave = prompt("Ingrese la clave");
+
+// if (edad > 18 && userClave === clave) {
+//   console.log("Puede pasar al sector VIP");
+// } else if (edad < 18) {
+//   console.log("No puede pasar, su edad es muy chico");
+// } else {
+//   console.log("No puede pasar al sector VIP");
+// }
