04dc1c4b39c24bd6e9ee1c9cee544564c5314e54
Juan Ma <juanmariamigliore@gmail.com>
2021-06-30 16:10:47 -0300

    Node

---
+++ Clase 15/node/app.js
@@ -0,0 +1,3 @@
+// Nos estamos llendo a buscar módulos. En este caso buscamos el módulo que se llama fs
+const fs = require('fs');
+fs.writeFileSync('nombres.txt', 'Juan Ma Migliore');
---
+++ Clase 15/node/explicacion.js
@@ -0,0 +1,10 @@
+// function requiree(module) {
+//   if (module === 'fs') {
+//     return {
+//       name: 'Modulo FS'
+//     };
+//   }
+// }
+
+// const fs = requiree('fs');
+// console.log(fs.name);
