13bc5de0836d25929f872e952b3a02a25f493690
Juan Ma <juanmariamigliore@gmail.com>
2021-06-30 16:35:28 -0300

    Node

--- Clase 15/node/app.js
+++ Clase 15/node/app.js
@@ -1,3 +1,7 @@
 // Nos estamos llendo a buscar módulos. En este caso buscamos el módulo que se llama fs
 const fs = require('fs');
+const utils = require('./utils.js');
+console.log(utils);
+
 fs.writeFileSync('nombres.txt', 'Juan Ma Migliore');
+fs.appendFileSync('nombres.txt', 'Danilo');
---
+++ Clase 15/node/utils.js
@@ -0,0 +1,5 @@
+console.log("Juan Ma");
+const nombre = "Juan";
+const apellido = "Migliore";
+
+module.exports = nombre;
