e44ba0b222e04390632e4743c304b11b665743bf
Juan Maria Migliore <juanmamig@Juans-MacBook-Pro.local>
2021-07-07 14:11:42 -0300

    EJS boilerplate

---
+++ project-ejs/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const products = require('./utils/products');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine',  'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) => {
+    res.render('index', {
+        title: 'E-commerce',
+        products
+    })
+});
+
+app.get('/contacto', (req,res) => {
+    res.render('pages/contacto', {
+        title: 'E-commerce | Contacto'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Puerto 3000');
+})
+
---
+++ project-ejs/utils/products.js
@@ -0,0 +1,10 @@
+const products = [
+  {
+    id: 0,
+    name: 'Mini Pymer',
+    price: 2700,
+    color: 'white'
+  }
+]
+
+module.exports = products;
